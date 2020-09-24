a482c26fdf893630432bc0623c329ca9f11624d9
unknown <matejzmaj12@gmail.com>
2020-09-24 15:47:14 +0200

    first components push

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyC5HIJ2_03s0szDo7Eb1-NLu_LX36_26ms',
+  authDomain: 'whatsapp-mern-9dc3c.firebaseapp.com',
+  databaseURL: 'https://whatsapp-mern-9dc3c.firebaseio.com',
+  projectId: 'whatsapp-mern-9dc3c',
+  storageBucket: 'whatsapp-mern-9dc3c.appspot.com',
+  messagingSenderId: '759786344902',
+  appId: '1:759786344902:web:9bd881964d6a29d5dbe2c7',
+};
